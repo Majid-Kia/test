fcf97b7c6f735e85e9ceba6db4e17a5633258156
Majid kia <majidkia.dev@gmail.com>
2023-02-15 22:39:27 +0330

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [partytown()]
+  site: "https://majid-kia.github.io/",
+  base: "/test",
+  integrations: [partytown()],
 });
